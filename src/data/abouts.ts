ac5777ae6ba34eaa7a86ae4c67a575ddd6adb3e0
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-04-04 23:07:11 +0100

    :sparkles: Add About content data file

---
+++ src/data/abouts.ts
@@ -0,0 +1,28 @@
+export interface About {
+  name: string;
+  position: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
+export const abouts: About[] = [
+  {
+    name: "Inês Casimiro",
+    position: "Recruiter",
+    description:
+      "Tenho uma paixão enorme por ajudar pessoas a encontrarem o seu lugar ideal no mercado de trabalho. Conto com vários anos de experiência no setor de recrutamento, e sei como é difícil encontrar o rumo certo. A Sidekick vem resolver esse problema – assegurar que cada pessoa não só encontra um emprego, mas inicia uma carreira gratificante.",
+    image:
+      "https://images.unsplash.com/photo-1631377307475-9acfa929b062?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/inês-casimiro/",
+  },
+  {
+    name: "Gonçalo Dias",
+    position: "Designer",
+    description:
+      "Sei o quão importante é o currículo – não apenas do ponto de vista visual, mas também no storytelling das tuas capacidades e experiências. O trabalho que fazemos na Sidekick vai para além disso. Desenvolvemos a tua marca pessoal, transmitimos a tua identidade de forma autêntica, e garantimos que te destacas no mercado de trabalho de forma única e profissional.",
+    image:
+      "https://images.unsplash.com/photo-1668112262164-56e782a6e07a?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/zalodias/",
+  },
+];
