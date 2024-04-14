4a0e4efbad344047288add605a23dfc2ec5319b2
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-04-14 15:44:11 +0100

    :memo: Update testimonials content copy

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -8,18 +8,18 @@ export interface Testimonial {
 export const testimonials: Testimonial[] = [
   {
     quote:
-      "Tudo se tornou mais fácil com o acompanhamento que me fizeram: ganhei confiança em mim, nas minhas competências e capacidades, e isso refletiu-se no processo de entrevistas com a minha atual empresa.",
+      "A entrada no mercado de trabalho foi um tanto assustadora, sentia alguma insegurança nas entrevistas e incerteza quanto às vagas a que me devia candidatar. Felizmente consegui ganhar confiança que precisava e dominar as minhas entrevistas.",
     avatar:
-      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D",
+      "https://media.licdn.com/dms/image/C4D03AQHVr318sAXp7w/profile-displayphoto-shrink_400_400/0/1610558276899?e=1718841600&v=beta&t=OR2jFstsWmTXFvW9oYT08myqdgViyOnCfJgZ_FmjCxs",
     name: "Rita Coelho",
-    position: "Software Engineer, Feedzai",
+    position: "Campaign Specialist, VML",
   },
   {
     quote:
-      "Tudo se tornou mais fácil com o acompanhamento que me fizeram: ganhei confiança em mim, nas minhas competências e capacidades, e isso refletiu-se no processo de entrevistas com a minha atual empresa.",
+      "Além da tradicional preparação de respostas para uma entrevista, ensinam também a manter a calma, criar empatia e recuperar o domínio da entrevista que é tão fácil de perder no meio do nervosismo.",
     avatar:
-      "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "João Grilo",
-    position: "Software Engineer, Feedzai",
+      "https://images.unsplash.com/photo-1615109398623-88346a601842?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Miguel Santos",
+    position: "Software Engineer, Revolut",
   },
 ];
